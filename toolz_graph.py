b8fb7bcd69f17f368126f84cfda2a88cac1af6c7
James Michael DuPont <jamesmikedupont+build@gmail.com>
2019-07-16 07:41:20 -0400

    frequencies

--- toolz_graph.py
+++ toolz_graph.py
@@ -1,4 +1,5 @@
 from toolz.curried import *
+from collections import Counter
 import json
 import click
 import pprint
@@ -21,19 +22,57 @@ def pushdown(collection, key, obj):
         key + "_obj" : obj
     }
 
+@curry
+def attach_name(field_name, obj):
+    target =  obj[field_name + '_obj']
+    value =  obj['_id_obj']    
+    target[field_name]= value
+    return target
+
+def keys(obj):
+    return list(obj.keys())
+
+def join_field(field_name, nodes):
+    results = map(
+        attach_name(field_name), 
+        keyjoin(
+        field_name,
+        filterfalse(
+            get_in(field_name),
+            map(pushdown('_'+ field_name, field_name),
+                filter(_hasattr(field_name),nodes)
+            )
+        ),
+        '_id',
+        map(pushdown('_node','_id'),
+            nodes)
+    ))
+    return results
+
+def explode(x):
+    for y in x:
+        yield y 
+def foo(*args):
+    res = Counter()
+    for y in args:
+        if not y:
+            continue
+        for x in y:
+            v = res[x]
+            
+            #print(x, v)
+            res[x] += v + 1
+    return res
+    
 @click.command()
 @click.argument('input-file', type=click.File('r'))
 def main (input_file):
     data = json.load(input_file)
     nodes = data['nodes']
-    #out_degrees = countby(apply(get_in(['_type'])),  nodes)
 
-    unql = keyjoin('unql',
-                   filterfalse(get_in('unql'),
-                               map(pushdown('_unql', 'unql'),filter(_hasattr('unql'),nodes))),
-                                      '_id',
-                                      map(pushdown('_node','_id'),nodes))
-    pprint.pprint(list(unql))
-    
+    #unql = join_field('unql',nodes)
+    name = join_field('name',nodes)
+    pprint.pprint(frequencies(list(concat(map(keys,name)))))
+
 if __name__ == '__main__':
     main()
